7cc7b928c87df956d74fc558894f9fad97e3b8a8
Linda Kounechongprasert <lindak@Lindas-MacBook-Pro.local>
2021-10-18 19:36:40 -0700

    added route paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Products from './Components/Products';
+import ProductDetails from './Components/ProductDetails';
+import Cart from './Components/Cart';
+import Checkout from './Components/Checkout';
+
 
 function App() {
   return (
     <Router>
         <div className="App">
-      
+          
+        <Switch>
+          <Route path="/">
+            <Products />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails />
+          </Route>
+          <Route path="/cart">
+            <Cart  />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
         </div>
     </Router>
    
