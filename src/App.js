524a2ec7b8d2860affb6d68dd267794099d68b26
Linda Kounechongprasert <lindak@Lindas-MacBook-Pro.local>
2021-10-18 18:40:45 -0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+        <div className="App">
+      
+        </div>
+    </Router>
+   
+  );
+}
+
+export default App;
